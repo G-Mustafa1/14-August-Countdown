3e50ee6ecce291429ea72b9bf749c85e86f45c84
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2024-08-05 23:31:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let day = document.getElementById('days');
+let hour = document.getElementById('hours');
+let minut = document.getElementById('minutes');
+let second = document.getElementById('second');
+setInterval(function () {
+   let currentDate = new Date().getTime();
+   let augest14 = new Date('14 aug 2024').getTime();
+   let total = augest14 - currentDate;
+   let days = Math.floor(total / (1000 * 60 * 60 * 24));
+   day.innerHTML=days
+   let hours = Math.floor(total % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+   hour.innerHTML=hours
+   let minuts = Math.floor(total % (1000 * 60 * 60) / (1000 * 60));
+   minut.innerHTML=minuts
+   let seconds = Math.floor(total % (1000 * 60) / (1000));
+   second.innerHTML=seconds
+}, 1000)
